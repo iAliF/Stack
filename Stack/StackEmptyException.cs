c7a871c8d813f0fd54e224e78c75cfd23c3bba66
Ali <the.alif.dev@gmail.com>
2023-11-02 18:30:54 +0330

    Add 'StackEmptyException' class

---
+++ Stack/StackEmptyException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Stack
+{
+    [Serializable]
+    public class StackEmptyException : Exception
+    {
+        public StackEmptyException() : base("Stack is empty")
+        {
+        }
+    }
+}
