b244e0d924cb7599ce7543c9e3f5f99e0b5fd1a9
Ali <the.alif.dev@gmail.com>
2023-11-02 18:28:33 +0330

    Add constructor method & properties

--- Stack/Stack.cs
+++ Stack/Stack.cs
@@ -1,6 +1,22 @@
 ﻿namespace Stack
 {
-    public class Stack
+    public class Stack<TType>
     {
+        private const int DefaultMaxSize = 100;
+
+        private readonly int _maxSize;
+        private int _top = -1;
+        private readonly TType[] _array;
+
+
+        public Stack() : this(DefaultMaxSize)
+        {
+        }
+
+        public Stack(int maxSize)
+        {
+            _maxSize = maxSize;
+            _array = new TType[maxSize];
+        }
     }
 }
