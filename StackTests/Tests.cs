95e11ca77502fa4c8e30ceecc64f0654341f2d67
Ali <the.alif.dev@gmail.com>
2023-11-03 20:35:17 +0330

    Add StackTests project

---
+++ StackTests/Tests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using NUnit.Framework;
+
+using Stack;
+
+namespace StackTests
+{
+    [TestFixture]
+    public class Tests
+    {
+        private Stack<int> _stack;
+        private const int MaxSize = 10;
+
+        [SetUp]
+        public void Init()
+        {
+            _stack = new Stack<int>(MaxSize);
+        }
+    }
+}
